e5e0c0ef29505e410e467e625e4383feb38a5a42
hupenghui <1049970895@qq.com>
2020-04-17 00:42:15 +0800

    spring security
    spring security

---
+++ spring-mvc/src/main/java/com/hph/config/MethodSecurity.java
@@ -0,0 +1,12 @@
+package com.hph.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 启用基于注解的方法安全性
+ * @author 10499
+ */
+@Configuration
+@EnableGlobalMthodSecurity()
+public class MethodSecurity {
+}
