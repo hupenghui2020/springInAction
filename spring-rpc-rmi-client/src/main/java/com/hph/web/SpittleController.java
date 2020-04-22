7c5ed7c24beb5a150e5a75b84ac478a3c4399fe3
hph <1049970895@qq.com>
2020-04-22 18:25:57 +0800

    更新

--- spring-rpc-rmi-client/src/main/java/com/hph/web/SpittleController.java
+++ spring-rpc-rmi-client/src/main/java/com/hph/web/SpittleController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 
 
+/**
+ * @author hph
+ */
 @Controller
 @RequestMapping(value = "/spittles")
 public class SpittleController {
